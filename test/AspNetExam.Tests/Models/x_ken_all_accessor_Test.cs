d35dcaaffb8fc86d685a8299ffba3bcc92fae251
Kouji Matsui <k@kekyo.net>
2016-10-17 21:26:59 +0900

    Add UT for netcore (WIP)

---
+++ test/AspNetExam.Tests/Models/x_ken_all_accessor_Test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace AspNetExam.Models
+{
+    [TestFixture]
+    public class x_ken_all_accessor_Test
+    {
+        [Test]
+        public async Task FetchByNewZipCodeAsyncTestAsync()
+        {
+            
+        }
+    }
+}
---
+++ test/AspNetExam.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AspNetExam.Tests")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a98c9b0c-57bf-4936-9e3d-d01dd15f5177")]

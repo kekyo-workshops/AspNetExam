e023048b8a921761d711b4e432e5a7a73b0bfdce
Kouji Matsui <k@kekyo.net>
2016-10-18 22:57:42 +0900

    First approach SignalR (broken)

---
+++ src/AspNetExam/Hubs/KenAllHub.cs
@@ -0,0 +1,30 @@
+﻿using AspNetExam.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.AspNetCore.SignalR.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetExam.Hubs
+{
+    [HubName("ken_all_hub")]
+    public class KenAllHub : Hub
+    {
+        private readonly IHostingEnvironment _hosting;
+
+        public KenAllHub(IHostingEnvironment hosting)
+        {
+            _hosting = hosting;
+        }
+
+        public x_ken_all[] GetByZipCode(int zipCode)
+        {
+            // 郵便番号辞書から検索する
+            var records = x_ken_all_accessor.FetchByNewZipCodeAsync(
+                _hosting.WebRootPath, zipCode).Result;
+            return records;
+        }
+    }
+}
